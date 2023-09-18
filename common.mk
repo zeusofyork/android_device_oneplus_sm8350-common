b811325fa12acfc52069d52b301f67a4ed3e8c33
TechMatrixx <techmatrixx34@gmail.com>
2023-09-18 18:00:17 +0530

    sm8350-common: Add support for ViPER4AndroidFX
    Ref: https://github.com/TogoFire/packages_apps_ViPER4AndroidFX

--- common.mk
+++ common.mk
@@ -469,6 +469,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# V4A
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v30.so
